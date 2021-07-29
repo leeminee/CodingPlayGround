691827c63e1580d658a36fa97d8575617e9602f5
Minee <mllee@ipageon.com>
2021-07-29 17:01:48 +0900

    프론트 복습

--- async/promise.js
+++ async/promise.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /** Promise
  *  : 자바스크립트 안에 내장되어있는 object
@@ -10,62 +10,63 @@
 // 1. Producer
 // 💡 when new Promise is create, the executor runs automatically.
 const promise = new Promise((resolve, reject) => {
-    // doing some heavy work (network, read files)
-    console.log('doing something...');
-    setTimeout(() => {
-        resolve('ellie');
-        // reject(new Error('no network'));
-    }, 2000);
+  // doing some heavy work (network, read files)
+  console.log("doing something...");
+  setTimeout(() => {
+    resolve("ellie");
+    // reject(new Error('no network'));
+  }, 2000);
 });
 
 // 2. Consumers: then(성공), catch(실패), finally(디폴트)
 // Promise 가 성공적으로 수행되어 resolve가 수행되었을때 then에서 받게된다.
-promise.then((value) => {
+promise
+  .then((value) => {
     console.log(value);
-})
-.catch(error => {
+  })
+  .catch((error) => {
     console.log(error);
-})
-.finally(() => {
-    console.log('finally');
-});
+  })
+  .finally(() => {
+    console.log("finally");
+  });
 
 // 3. Promise chaining
-const fetchNumber = new Promise((resolve, reject) =>{
-    setTimeout(() => resolve(1), 1000);
+const fetchNumber = new Promise((resolve, reject) => {
+  setTimeout(() => resolve(1), 1000);
 });
 
 fetchNumber
-.then(num => num * 2)
-.then(num => num * 3)
-.then(num => {
+  .then((num) => num * 2)
+  .then((num) => num * 3)
+  .then((num) => {
     return new Promise((resolve, reject) => {
-        setTimeout(() => resolve(num -1), 1000);
+      setTimeout(() => resolve(num - 1), 1000);
     });
-})
-.then(num => console.log(num));
+  })
+  .then((num) => console.log(num));
 
 // 4. Error Handling
-const getHen = () => 
-    new Promise((resolve, reject) => {
-        setTimeout(() => resolve('🐓'), 1000);
-    });
-const getEgg = hen => 
-    new Promise((resolve, reject) => {
-        setTimeout(() => reject(new Error(`error! ${hen} => 🥚`)), 1000);
-    });
-const cook = egg => 
-    new Promise((resolve, reject) => {
-        setTimeout(() => resolve(`${egg} => 🍳`), 1000);
-    });
+const getHen = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => resolve("🐓"), 1000);
+  });
+const getEgg = (hen) =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => reject(new Error(`error! ${hen} => 🥚`)), 1000);
+  });
+const cook = (egg) =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => resolve(`${egg} => 🍳`), 1000);
+  });
 // getHen()
 // .then(hen => getEgg(hen))
 // .then(egg => cook(egg))
 // .then(meal => console.log(meal));
 getHen()
-.then(getEgg)
-.catch(error => {
-    return '🍞';
-})
-.then(cook)
-.then(console.log);
+  .then(getEgg)
+  .catch((error) => {
+    return "🍞";
+  })
+  .then(cook)
+  .then(console.log);
--- function.js
+++ function.js
@@ -1,118 +1,121 @@
-'use strict';
+"use strict";
 // 💡 1. Function declaration
 function printHello() {
-    console.log('Hello');
+  console.log("Hello");
 }
 printHello();
 
 function log(message) {
-    console.log(message);
+  console.log(message);
 }
 
-log('Minee');
-
+log("Minee");
 
 // 💡 2. Parameters
 // premitive parameters : passed by value
 // object parameters : passed by reference
 function changeName(obj) {
-    obj.name = 'coder';
+  obj.name = "coder";
 }
-const ellie = {name : 'ellie'};
+const ellie = { name: "ellie" };
 changeName(ellie);
 console.log(ellie);
 
 // 💡 3. Default parameters (added in ES6)
-function showMessage(message, from = 'unkonwn') {
-    console.log(`${message} by ${from}`);
+function showMessage(message, from = "unkonwn") {
+  console.log(`${message} by ${from}`);
 }
 
 showMessage("Hi!");
 
 //💡 4. Rest parameters (added in ES6)
 function printAll(...args) {
-    for (let i = 0 ; i < args.length; i++) {
-        console.log(args[i]);
-    }
+  for (let i = 0; i < args.length; i++) {
+    console.log(args[i]);
+  }
 
-    for(let arg of args) {
-        console.log(arg);
-    }
+  for (let arg of args) {
+    console.log(arg);
+  }
 
-    args.forEach((arg) => console.log(arg))
+  args.forEach((arg) => console.log(arg));
 }
 
-printAll('dream', 'coding', 'ellie');
+printAll("dream", "coding", "ellie");
 
 // 💡 5. Local scope
-let globalMessage = 'global'; // global variable
+let globalMessage = "global"; // global variable
 function printMessage() {
-    let message = 'hello';
-    console.log(message); // local variable
-    console.log(globalMessage);
-    // function printAnother() {
-    //     console.log(message);
-    //     let childMessage = 'hello';
-    // }
-    // console.log(childMessage);
+  let message = "hello";
+  console.log(message); // local variable
+  console.log(globalMessage);
+  // function printAnother() {
+  //     console.log(message);
+  //     let childMessage = 'hello';
+  // }
+  // console.log(childMessage);
 }
 printMessage();
 
 // Callback function using function expression
-function randomQuiz(answer, printYes, printNo) { // 이렇게 함수내부에서 또다른 함수를 사용하는것을 콜백함수라고 한다?
-    if (answer === 'love you') {
-        printYes(); // 콜백함수
-    } else {
-        printNo(); // 콜백함수
-    }
+function randomQuiz(answer, printYes, printNo) {
+  // 이렇게 인자로 함수를 받아 내부에서 실행하는 것을 콜백함수라고 한다?
+  if (answer === "love you") {
+    printYes();
+  } else {
+    printNo();
+  }
 }
 
 // anonymous function
 const printYes = function () {
-    console.log('I love you too❤');
+  console.log("I love you too❤");
 };
 
 // named function
 // better debugging in debugger's stack traces
 const printNo = function print() {
-    console.log('No!');
+  console.log("No!");
 };
 
-randomQuiz('wrong',printYes, printNo);
-randomQuiz('love you' , printYes, printNo);
+randomQuiz("wrong", printYes, printNo);
+randomQuiz("love you", printYes, printNo);
 
 // ✨Arrow function
-// always anonymous 
-const simplePrint = () => console.log('simplePrint!');
-const add = (a,b) => a+b;
-const simpleMultiply = (a,b) => { // Arrow function 에서 이렇게 블럭을 사용할경우에는 반드시 return 값을 써 주어야함.
-    // do someting more
-    return a * b;
+// always anonymous
+const simplePrint = () => console.log("simplePrint!");
+const add = (a, b) => a + b;
+const simpleMultiply = (a, b) => {
+  // Arrow function 에서 이렇게 블럭을 사용할경우에는 반드시 return 값을 써 주어야함.
+  // do someting more
+  return a * b;
 };
 
 // IIFE : Immediately Invoked Function Expression
 // 함수를 바로 호출하고 싶을때 아래와 같이 사용한다.
 (function hello() {
-    console.log('IIFE');
-})();  
+  console.log("IIFE");
+})();
 
 // Fun quiz time💛
 // function calculate(command, a, b)
 // command: add, substract, divide, multiply, remainder
 const calculate = (command, a, b) => {
-    switch(command){
-        case 'add':
-            return a + b;
-        case 'substract':
-            return a - b; 
-        case 'divide':
-            return a / b;
-        case 'multiply':
-            return a * b;
-        case 'remainder':
-            return a % b; 
-        default:
-            return console.log('you have to input add or substract or divide or multiply or remainder.')
-    }
-}
-console.log(calculate('remainder', 3,4));
+  switch (command) {
+    case "add":
+      return a + b;
+    case "substract":
+      return a - b;
+    case "divide":
+      return a / b;
+    case "multiply":
+      return a * b;
+    case "remainder":
+      return a % b;
+    default:
+      return console.log(
+        "you have to input add or substract or divide or multiply or remainder."
+      );
+  }
+};
+console.log(calculate("remainder", 3, 4));
