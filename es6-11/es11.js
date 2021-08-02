9a8abd9eb9977d603eeba4585da54d293832d520
Minee <mingramer@gmail.com>
2021-08-02 22:02:33 +0900

    es11 문법 익시는중

--- es6-11/es11.js
+++ es6-11/es11.js
@@ -1,1 +1,56 @@
 'use strict';
+
+/**
+ * Optional Chaining (ES11)
+ * 
+ */
+{
+    const person1 = {
+        name: 'Wendy',
+        job: {
+            title: 'S/W Engineer',
+            manager: {
+                name: 'Bob',
+            },
+        },
+    };
+    const person2 = {
+        name: 'Bob',
+    };
+
+    //💩
+    {
+        function printManager(person) {
+            console.log(person.job.manager.name);
+        }
+        printManager(person1);
+        // printManager(person2);
+    }
+
+    // 💩
+    {
+        function printManager(person) {
+            console.log(person.job ? person.job.manager ? person.job.manager.name : undefined : undefined);
+        }
+        printManager(person1);
+        printManager(person2);
+    }
+
+    // 💩
+    {
+        function printManager(person) {
+            console.log(person.job && person.job.manager && person.job.manager.name);
+        }
+        printManager(person1);
+        printManager(person2);
+    }
+
+    // ✨
+    {
+        function printManager(person) {
+            console.log(person.job?.manager?.name);
+        }
+        printManager(person1);
+        printManager(person2);
+    }
+}
