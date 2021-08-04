e01b6b6c19acb5efbf19c65abeb84205516e2a85
Minee <mllee@ipageon.com>
2021-08-04 10:25:17 +0900

    es11 문법 연습

--- es6-11/es11.js
+++ es6-11/es11.js
@@ -1,56 +1,104 @@
-'use strict';
+"use strict";
 
 /**
  * Optional Chaining (ES11)
- * 
+ *
  */
 {
-    const person1 = {
-        name: 'Wendy',
-        job: {
-            title: 'S/W Engineer',
-            manager: {
-                name: 'Bob',
-            },
-        },
-    };
-    const person2 = {
-        name: 'Bob',
-    };
+  const person1 = {
+    name: "Wendy",
+    job: {
+      title: "S/W Engineer",
+      manager: {
+        name: "Bob",
+      },
+    },
+  };
+  const person2 = {
+    name: "Bob",
+  };
 
-    //💩
-    {
-        function printManager(person) {
-            console.log(person.job.manager.name);
-        }
-        printManager(person1);
-        // printManager(person2);
+  // 💩
+  {
+    function printManager(person) {
+      console.log(person.job.manager.name);
     }
+    printManager(person1);
+    // printManager(person2);
+  }
 
-    // 💩
-    {
-        function printManager(person) {
-            console.log(person.job ? person.job.manager ? person.job.manager.name : undefined : undefined);
-        }
-        printManager(person1);
-        printManager(person2);
+  // 💩
+  {
+    function printManager(person) {
+      console.log(
+        person.job
+          ? person.job.manager
+            ? person.job.manager.name
+            : undefined
+          : undefined
+      );
     }
+    printManager(person1);
+    printManager(person2);
+  }
 
-    // 💩
-    {
-        function printManager(person) {
-            console.log(person.job && person.job.manager && person.job.manager.name);
-        }
-        printManager(person1);
-        printManager(person2);
+  // 💩
+  {
+    function printManager(person) {
+      console.log(person.job && person.job.manager && person.job.manager.name);
     }
+    printManager(person1);
+    printManager(person2);
+  }
 
-    // ✨
-    {
-        function printManager(person) {
-            console.log(person.job?.manager?.name);
-        }
-        printManager(person1);
-        printManager(person2);
+  // ✨
+  {
+    function printManager(person) {
+      console.log(person.job?.manager?.name);
     }
+    printManager(person1);
+    printManager(person2);
+  }
+}
+
+/**
+ * Nullish Coalescing Operator (ES11)
+ *
+ */
+{
+  // Logical OR operator
+  // false : false, '', 0, null, undefined
+  {
+    const name = "Wendy";
+    const userName = name || "Guest";
+    console.log(userName);
+  }
+
+  {
+    const name = null;
+    const userName = name || "Guest";
+    console.log(userName);
+  }
+
+  // 💩
+  {
+    const name = "";
+    const userName = name || "Guest";
+    console.log(userName);
+
+    const num = 0;
+    const message = num || "Guest";
+    console.log(message);
+  }
+
+  // ✨
+  {
+    const name = "";
+    const userName = name ?? "Guest";
+    console.log(userName);
+
+    const num = 0;
+    const message = num ?? "undefined";
+    console.log(message);
+  }
 }
